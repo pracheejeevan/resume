7f9efd625e3d1a717661b52879547716d279b4d1
Jagat Jeevan Sahoo <kanhajeevan@gmail.com>
2015-08-16 14:29:05 +0530

    added Excellence Section

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,21 @@
+var setUpExcellence = function(){
+  $("#proffessional-skills").find(".skill").each(function(){
+    var skillPercent = $(this).data("skill-completed");
+    $(this).find(".skill-completed").animate({
+      width: skillPercent+'%'
+    }, 810);
+    $(this).find(".skill-text").append(" (" + skillPercent+ "%)");
+  });
+};
+
+var startAllFunctions = function(){
+  setUpExcellence();
+};
+
 $(window).load(function(){
   $(".loader").fadeOut("slow", function(){
-    $(".container").fadeIn("slow");
+    $(".container").fadeIn("slow", function(){
+      startAllFunctions();
+    });
   });
 });
